a4321001329b054efbbbab1ae6276b21d34511ed
David Torres Pascual <davidtorresp5@gmail.com>
2016-11-08 10:22:26 +0100

    Update app.R

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(ggExtra)
 library(shinythemes)
 library(dplyr)
+library(tidyr)
 
 data <- read.csv('Pokemon.csv')
 data$Generation <- as.factor(data$Generation)
@@ -13,74 +14,81 @@ ui <- tagList(
   
   navbarPage(
     theme = shinytheme("flatly"),"PokeStats",
-    tabPanel("Inicio",
+    tabPanel("Home",
              sidebarPanel(
-               h4("Una aplicación para analizar datos del videojuego Pokémon"),
-               h5("Creada por David Torres Pascual (@DavidTweid)")
+               h4("An app that analyze data from Pokemon"),
+               h5("Created by David Torres Pascual (@DavidTweid)"),
+               h5("2016")
              ),
              mainPanel(
                img(src="https://s14.postimg.org/tdpnz4d5d/Poke_Stats.png", align = "center"),
-               h3("Comparación de atributos"),
-               h4("En este apartado puedes comparar los atributos de los diferentes pokémon 
-                  según su tipo, generación o entre legendario/no legendario"),
-               h3("Correlación entre atributos"),
-               h4("En este apartado puedes analizar la correlación entre
-                  diferentes atributos.")
+               h3("Search a Pokemon"),
+               h4("Explore the stats of your favourite Pokemon."),
+               h3("Comparison"),
+               h4("Compare different attributes based on type, generation or legendary status."),
+               h3("Correlation"),
+               h4("Analyze the correlation bewteen attributes.")
                
              )
       
     ),
+    tabPanel("Search a Pokemon",
+             sidebarPanel(
+               h4("Introduce the name of the Pokemon"),
+               textInput("name", label= "Name:", value = "Venonat")
+               ),
+             mainPanel(
+               h3("Pokémon statistics:"),
+               plotOutput("poke")
+               )
+    ),
     
-    
-    tabPanel("Comparación de atributos",
+    tabPanel("Comparison",
              
              sidebarPanel(
-               h4("Comparación de atributos de tipos de Pokemon"),
-               h5("En este apartado puedes comparar los atributos de los 
-                  diferentes pokémon según su tipo, generación o entre 
-                  legendario/no legendario"),
-               selectInput("variable", "Comparar por:",
-                           list("Tipo" = "Type1",
-                                "Generación" = "Generation",
-                                "Legendario" = "Legendary")),
+               h4("Comparison"),
+               h5("Compare different attributes based on type, generation or legendary status"),
+               selectInput("variable", "Compare by:",
+                           list("Type" = "Type1",
+                                "Generation" = "Generation",
+                                "Legendary" = "Legendary")),
                
-               selectInput("variable2", "Atributo a comparar:",
+               selectInput("variable2", "Attribute:",
                            list("HP" = "HP", 
-                                "Ataque" = "Attack", 
-                                "Defensa" = "Defense",
-                                "Ataque Especial" = "SpAtk",
-                                "Defensa Especial" = "SpDef",
-                                "Velocidad" = "Speed"),
+                                "Attack" = "Attack", 
+                                "Defense" = "Defense",
+                                "Special Attack" = "SpAtk",
+                                "Special Defense" = "SpDef",
+                                "Speed" = "Speed"),
                            selected ="HP")
              ),
              mainPanel(
                tabsetPanel(
-                 tabPanel("Comparación",plotOutput("plot")),
-                 tabPanel("Histograma del atributo",plotOutput("histograma"))
+                 tabPanel("Comparison",plotOutput("plot")),
+                 tabPanel("Histogram",plotOutput("histograma"))
                )
              )
     ),
     
-    tabPanel("Correlación de atributos",
+    tabPanel("Correlation",
              sidebarPanel(  
-               h4("Correlación entre atributos"),
-               h5("En este apartado puedes analizar la correlación entre
-                  diferentes atributos."),
-               selectInput("variable3", "Atributo 1:",
+               h4("Correlation"),
+               h5("Analyze the correlation bewteen attributes."),
+               selectInput("variable3", "Attribute 1:",
                            list("HP" = "HP", 
-                                "Ataque" = "Attack", 
-                                "Defensa" = "Defense",
-                                "Ataque Especial" = "SpAtk",
-                                "Defensa Especial" = "SpDef",
-                                "Velocidad" = "Speed"),
+                                "Attack" = "Attack", 
+                                "Defense" = "Defense",
+                                "Special Attack" = "SpAtk",
+                                "Special Defense" = "SpDef",
+                                "Speed" = "Speed"),
                            selected ="Speed"),
-               selectInput("variable4", "Atributo 2:",
+               selectInput("variable4", "Attribute 2:",
                            list("HP" = "HP", 
-                                "Ataque" = "Attack", 
-                                "Defensa" = "Defense",
-                                "Ataque Especial" = "SpAtk",
-                                "Defensa Especial" = "SpDef",
-                                "Velocidad" = "Speed"),
+                                "Attack" = "Attack", 
+                                "Defense" = "Defense",
+                                "Special Attack" = "SpAtk",
+                                "Special Defense" = "SpDef",
+                                "Speed" = "Speed"),
                            selected ="Attack")
              ),
              
@@ -91,20 +99,36 @@ ui <- tagList(
              
              ),
   
-  tabPanel("Información",
+  tabPanel("Information",
     mainPanel(
-      h3("Información"),
-      h5("Esta aplicación está realizada con datos procedentes de https://www.kaggle.com/abcsds/pokemon.
-        Gracias a Nintendo, GameFreak, bulbapedia y a todos los desarrolladores y usuarios.")
+      h3("Information"),
+      h5("This app has been created using data from https://www.kaggle.com/abcsds/pokemon."),
+      h5("Thanks to Nintendo, GameFreak, bulbapedia, developers and users.")
     )
-)
-)
-)
+  )
+))
+
 
 
 
 server <- function(input, output) {
   
+  filter1 <- reactive({
+    data %>%
+    select(Name, HP, Attack, Defense, SpAtk, SpDef, Speed) %>%
+    filter(Name %in% as.character(input$name)) %>%
+    gather(Clave, Valor) %>%
+    slice(2:7)
+  })
+  
+  output$poke <- renderPlot({
+
+    b <- ggplot(filter1(), aes(factor(Clave), as.numeric(Valor)))
+    b2 <- b + geom_bar(stat="identity", color="#2F6CB4",fill="#FFCB05") + theme_minimal()
+    b3 <- b2 + labs(y = "Points", x = "Attribute")
+    print(b3)
+  })
+
   
   output$plot <- renderPlot({
     p <- ggplot(data, aes_string(x=input$variable, y=input$variable2)) #+ coord_flip()
